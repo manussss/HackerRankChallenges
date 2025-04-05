943236ca87a2cb3df6c5abd6c97f3b51f523731e
ebranquinho <branquinhomanu@gmail.com>
2025-04-05 11:13:50 -0300

    add array vs list

---
+++ src/Algorithms/ProgrammingChallenges/ManningAlgorithms/ArrayVsList.cs
@@ -0,0 +1,21 @@
+/*
+
+            Arrays    Listas
+Leitura    |  O(1)  | O(N)
+Inserção   |  O(N)  | O(1)
+Eliminação |  O(N)  | O(1)
+
+O(N) -> Tempo de execução linear
+O(1) -> Tempo de execução constante
+
+Existem dois tipos de acesso: aleatório e o sequencial.
+
+Sequencial significa ler elementos um por um, comecando sempre pelo primeiro.
+
+Listas encadeadas só podem lidar com acesso sequencial. Se você quiser ler o 
+décimo elemento de uma lista encadeada, primeiro precisará ler os nove anteriores para chegar
+ao endereço do décimo.
+
+O aleatório permite que você pule direto para o décimo elemento.
+
+*/
