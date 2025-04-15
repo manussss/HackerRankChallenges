fd14365b697e3837c10008bfd7c0dd636271051f
ebranquinho <branquinhomanu@gmail.com>
2025-04-15 20:54:57 -0300

    add bfs and dfs scrutctures

---
+++ src/Algorithms/ProgrammingChallenges/ManningAlgorithms/BreadthFirstSearch.cs
@@ -0,0 +1,11 @@
+namespace ProgrammingChallenges.ManningAlgorithms;
+
+/*
+Breadth-First Search (BFS) explores all nodes at the current level before moving 
+on to the next level. It’s useful for problems where you need to find the shortest 
+path between two nodes.
+*/
+public class BreadthFirstSearch
+{
+
+}
---
+++ src/Algorithms/ProgrammingChallenges/ManningAlgorithms/DepthFirstSearch.cs
@@ -0,0 +1,11 @@
+namespace ProgrammingChallenges.ManningAlgorithms;
+
+/*
+Depth-First Search (DFS) explores as far down a branch of a tree or graph as 
+possible before backtracking. It goes deep into one branch before trying another, 
+making it ideal for problems where you need to explore all possible paths.
+*/
+public class DepthFirstSearch
+{
+
+}
