db1de12a23a78f887dd12155ade10ded1462adb6
ebranquinho <branquinhomanu@gmail.com>
2025-10-16 07:16:22 -0300

    add summary

--- src/Algorithms/ProgrammingChallenges/LeetCode/15-ValidAnagram.cs
+++ src/Algorithms/ProgrammingChallenges/LeetCode/15-ValidAnagram.cs
@@ -5,6 +5,12 @@ public static class ValidAnagram
     /*
     1) Given two strings s and t, return true if t is an anagram of s, and false otherwise
         Using all of the characters in 's'we can create 't'
+
+    2) Create two dics for each input
+    3) map 's' and how many ocurrences it has
+    4) map 't' and how many ocurrences it has
+    5) foreach 's', check if 't' has its key (if it doesnt, then return false)
+        5.1) also check if t's value is the same as s's value, if its not, the return false
     
     Input: s = "anagram", t = "nagaram"
     Output: true
@@ -38,10 +44,10 @@ public static class ValidAnagram
 
         foreach (var sMapItem in sMap)
         {
-            if (!tMap.ContainsKey(sMapItem.Key))
+            if (!tMap.TryGetValue(sMapItem.Key, out int value))
                 return false;
 
-            if (tMap[sMapItem.Key] != sMapItem.Value)
+            if (value != sMapItem.Value)
                 return false;
         }
 
